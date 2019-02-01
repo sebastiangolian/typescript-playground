4a0d1c73c261591c370750763d4e31629d17daf9
Sebastian Golian <sebastiangolian@gmail.com>
2019-02-01 21:59:24 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 import { User } from "./classes/User.js";
 let user = new User("Jan", "Kowalski");
 const test = document.getElementById("test");
-test.innerHTML = user.getFullName();
+test.innerHTML += user.getFullName();
